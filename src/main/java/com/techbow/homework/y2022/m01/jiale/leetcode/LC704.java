ffb151ee6387ac420e23f0ab74bdb26ae08e50e9
JialeDai <jd4678@nyu.edu>
2022-01-01 22:44:01 -0500

    LC704

---
+++ src/main/java/com/techbow/homework/y2022/m01/jiale/leetcode/LC704.java
@@ -0,0 +1,66 @@
+package com.techbow.homework.y2022.m01.jiale.leetcode;
+
+/**
+ * binary search
+ */
+public class LC704 {
+    /**
+     * left = 0; right = len - 1
+     * left <= right
+     * mid = right -1;
+     * mid - left + 1;
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * left = 0, right = len -1;
+     * left + 1 < right
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        if (nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+}
